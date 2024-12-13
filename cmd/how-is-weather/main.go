e6d4d8ab15a839c3d74f9f6bb9cc8acf7fadfa73
Borzoo Moazami <borzoomoazami94@gmail.com>
2024-12-13 06:56:20 +0330

    feat: start listening on a port

--- cmd/how-is-weather/main.go
+++ cmd/how-is-weather/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("couldn't load .env file:\n%v\n", err.Error())
+	}
+
+	fmt.Printf("Listening on port %v...\n", os.Getenv("SERVER_LISTENING_PORT"))
+	err = http.ListenAndServe(fmt.Sprintf(":%v", os.Getenv("SERVER_LISTENING_PORT")), nil)
+	if err != nil {
+		log.Fatalf("server couldn't start:\n%v\n", err.Error())
+	}
 }
